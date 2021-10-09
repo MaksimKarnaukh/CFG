6377ff3b89d1bd2cbb1fc5a0aba27dc9555aecf0
Maksim Karnaukh <s0201604@ua.ad.ac.be>
2021-10-09 22:10:07 +0200

    first commit, added main cfg class

---
+++ CFG.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Maksim Karnaukh on 9/10/2021.
+//
+
+#include "CFG.h"
---
+++ CFG.h
@@ -0,0 +1,14 @@
+//
+// Created by Maksim Karnaukh on 9/10/2021.
+//
+
+#ifndef CFG1_CFG_H
+#define CFG1_CFG_H
+
+
+class CFG {
+
+};
+
+
+#endif //CFG1_CFG_H
